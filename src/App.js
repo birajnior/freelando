a4bd265d3d3b98bf65c89c6f5bc01bd2f8a7f962
birajnior <ubiratanbernardo@gmail.com>
2024-09-04 17:16:07 -0300

    Adicionando React-grid-system

--- src/App.js
+++ src/App.js
@@ -5,23 +5,30 @@ import { Footer } from "./components/footer/Footer";
 import { ProvedorTema } from "./components/ProvedorTema/ProvedorTema";
 import { Tipografia } from "./components/Tipografia/Tipografia";
 import { Cabecalho } from "./components/Cabecalho/Cabecalho";
+import { Col, Container, Row } from "react-grid-system";
 
 function App() {
   return (
     <ProvedorTema>
       <Estilos />
       <Cabecalho />
-      <Card>
-        <Tipografia variante="h3" componente="h1">
-          Crie seu cadastro
-        </Tipografia>
-        <Tipografia variante="body" componente="body">
-          Crie seu perfil gratuitamente para começar a trabalhar com os melhores
-          freelancers. Em seguida, você poderá dar mais detalhes sobre suas
-          demandas e sobre sua forma de trabalho.
-        </Tipografia>
-        <CampoTexto titulo="Nome Completo" />
-      </Card>
+      <Container>
+        <Row justify="center">
+          <Col lg={6}>
+            <Card>
+              <Tipografia variante="h3" componente="h1">
+                Crie seu cadastro
+              </Tipografia>
+              <Tipografia variante="body" componente="body">
+                Crie seu perfil gratuitamente para começar a trabalhar com os
+                melhores freelancers. Em seguida, você poderá dar mais detalhes
+                sobre suas demandas e sobre sua forma de trabalho.
+              </Tipografia>
+              <CampoTexto titulo="Nome Completo" />
+            </Card>
+          </Col>
+        </Row>
+      </Container>
       <Footer subtitulo="Desenvolvido por Alura. Projeto fictício sem fins comerciais." />
     </ProvedorTema>
   );
